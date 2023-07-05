bd1cc4fa20e4e87ed529a506004955aa5be88e28
Kumar_Anshuman <kanshuman6484@gmail.com>
2023-07-05 13:32:12 +0530

    Added new files

--- Javascript/Arrays.js
+++ Javascript/Arrays.js
@@ -16,7 +16,7 @@ console.log(marks[0])                       //changing the value of an array
 */
 
 // Array methods
-
+/*
 let num = [1, 2, 5, 84, 3]
 
 let b = num.toString()                      //converts b to string
@@ -33,3 +33,56 @@ console.log(num)
 
 // shift() -> pop an element from beginning of array. It also changes the array. Returns length of array
 // unshift() -> push an element at the beginning of array. It also changes the array. Returns length of array
+// delete num[0] -> deletes 1st element, doesn't change the length of array
+
+let num_2 = [74, 9, 83, 111]
+let newArray = num.concat(num_2)            //doesn't modify original array(num)
+console.log(newArray)
+
+// sort() -> sort the array in alphabetical order, modifies original array, it considers elements as strings
+console.log(num_2.sort())                   // prints 111, 74, 83, 9
+
+let comp = (a, b) => {
+    return a - b
+}
+console.log(num_2.sort(comp))               // prints in ascending order
+
+// reverse() -> reverse the array
+*/
+
+// Splice And Slice
+
+// syntax : array.splice(start, no.of elements to be deleted, keys to be added)
+
+/*
+let num = [1, 2, 5, 84, 3]
+num.splice(2, 2, 74, 73, 83, 826)           // returns deleted elements, modifies original array
+console.log(num)
+
+console.log(num.slice(2))                   //prints from index 2 to end
+console.log(num.slice(2, 4))                //prints from index 2 to index 3
+// slice doesn't modify original array
+*/
+let num = [1, 2, 3, 4, 5]
+/*
+//for Each loop
+num.forEach((key) => {
+    console.log(key * key)                    //prints square of each element
+})
+*/
+//Array from
+
+let name1 = "Amit"
+let arr = Array.from(name1)                  //converts string(anything) to array
+console.log(arr)
+
+//for of loop
+for (i of num) {
+    console.log(i)                          //prints array elements
+}
+
+//for in loop
+for (i in num) {
+    console.log(i)                          //prints index of array
+    console.log(num[i])                     //prints array elements
+}
