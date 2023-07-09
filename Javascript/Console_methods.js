5cb03fe43c6f82c35106c365cfa79d64f28759d4
Kumar_Anshuman <kanshuman6484@gmail.com>
2023-07-09 13:42:48 +0530

    Added new Js files

---
+++ Javascript/Console_methods.js
@@ -0,0 +1,31 @@
+// console.log(console)                 // prints all console functions
+
+// console.error("Error message")       // prints message in highlighted red colour
+
+// console.warn("Warning message")      // prints message in highlighted yellow colour
+
+// console.assert(5>3)                  // prints nothing if assertion is true
+// console.assert(5<3)                  // shows an error message if assertion is false
+
+// console.clear()                      // it clears the console
+
+/*
+obj = { a: 1, b: 2, c: 3 }
+
+console.table(obj)                      // prints obj in tabular form
+*/
+
+// console.info("This is an info")      // almost similar to console.log
+
+/*
+console.time("forloop")
+for (let i = 1; i <= 50; i++);
+console.timeEnd("forloop")              // prints time taken to run for loop
+
+console.time("whileloop")
+let i = 1
+while (i <= 50) {
+    i++
+}
+console.timeEnd("whileloop")            // prints time taken to run code b/w time("whileloop") and timeEnd("whileloop")
+*/
