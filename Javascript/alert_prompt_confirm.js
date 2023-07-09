f5aa86e1ac21b97d3fc72f9753cc1f51ee33adae
Kumar_Anshuman <kanshuman6484@gmail.com>
2023-07-09 14:43:38 +0530

    Added alert_prompt_confirm

---
+++ Javascript/alert_prompt_confirm.js
@@ -0,0 +1,16 @@
+alert("Enter a number")                     // prints an alert message
+
+// let a = prompt("Enter a number")
+
+let a = prompt("Enter a number", "100")     // default value is 100
+document.write(a)
+
+/*
+let write = confirm("Do you want to write it to the page")
+if(write){
+    document.write(a)
+}
+else{
+    document.write("Please allow me to write")
+}
+*/
