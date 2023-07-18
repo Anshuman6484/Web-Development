634b27a1ae5d1396583e5e086d092166b6179011
Kumar_Anshuman <kanshuman6484@gmail.com>
2023-07-18 15:00:45 +0530

    Added react

--- React/vite-project/src/App.tsx
+++ React/vite-project/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <h1>My name is Kumar Anshuman</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
